91b515e602a5f8097582f1553bf5154045b1e126
ramigyt-rgb <ramigyt@gmail.com>
2026-06-12 17:26:13 -0300

    Update fut_app.py

--- fut_app.py
+++ fut_app.py
@@ -72,12 +72,6 @@ if st.button("💾 Guardar señal"):
     nueva_senal = pd.DataFrame([{
 
         "fecha": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        
-estrategia = st.sidebar.selectbox(
-        "Estrategia",
-        ["Tendencia + Liquidez"],
-        key="estrategia_select"
-)
 
         "estrategia": estrategia,
 
