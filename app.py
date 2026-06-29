4292bb3fcab09a6ed9959408093137ad41242a6b
ramigyt-rgb <ramigyt@gmail.com>
2026-06-29 11:57:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,25 @@
 # APP PRINCIPAL
 # =========================
 import streamlit as st
+
 st.set_page_config(
+
     page_title="BTCUSDT Futures Dashboard Pro",
+
     layout="wide",
+
     initial_sidebar_state="expanded"
+
 )
+
 from login import check_access
 
 if check_access():
+
     from ui import render_dashboard
+
     render_dashboard()
+
 else:
-    st.info("Ingrese la clave de acceso para abrir el dashboard.")
+
+    st.stop()
